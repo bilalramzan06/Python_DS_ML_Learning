0e2c0c2da34429942c6baf8343fbbc8f0d6e35f1
Bilal Ramzan <bilalramzan102@gmail.com>
2023-07-18 14:10:59 +0500

    updated

---
+++ 01_Introduction_to_Python/Chapter_01_Python_Basics/02_add-comments.py
@@ -0,0 +1,7 @@
+"""To add comments to your Python script, you can use the # tag. These comments are not run as Python code, so they will not influence your result."""
+
+# Division
+print(5 / 8)
+
+# addition
+print(7 + 10)
---
+++ 01_Introduction_to_Python/Chapter_01_Python_Basics/06_other-variable-types.py
@@ -0,0 +1,6 @@
+# Create a variable desc
+desc = "compound interest"
+print(desc)
+# Create a variable profitable
+profitable = True
+print(profitable)
