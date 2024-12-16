a40e2b5db6cff800d9821c46c1fa4bc38f475589
Bilal Ramzan <121340921+bilalramzan06@users.noreply.github.com>
2024-12-17 00:34:44 +0500

    updated

---
+++ 01_Introduction_to_Python/casting.py
@@ -0,0 +1,6 @@
+# Specify a Variable Type
+# There may be times when you want to specify a type on to a variable. This can be done with casting. Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
